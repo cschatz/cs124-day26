1022b88a244aa000ec2d38d245c34932951be1f6
Colin Schatz <cschatz@mills.edu>
2021-12-02 10:36:31 -0800

    Add Path class

---
+++ src/Path.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+public class Path {
+	private ArrayList<GraphNode> pathNodes;
+	private int length;
+	
+	
+	// methods
+	public void addNode(GraphNode node, int distance) {
+		// add node to the end,
+		// increase length by distance
+	}
+	
+	public GraphNode getLastNode() {
+		return null; // placeholder
+	}
+}
