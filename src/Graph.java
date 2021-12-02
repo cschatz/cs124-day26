05ebe33cdf87fdb222c1d932e3f806a8722a5721
Colin Schatz <cschatz@mills.edu>
2021-12-02 10:32:22 -0800

    Initial commit

---
+++ src/Graph.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class Graph {
+	// map from names of cities to nodes
+	private HashMap<String, GraphNode> cityMap;
+	
+	// methods
+	public void addNode(String cityName) {
+		
+	}
+	
+	public void addEdge(String cityA, String cityB) {
+		
+	}
+	
+	public void findPath(String start, String end) {
+		
+	}
+	
+	
+}
---
+++ src/GraphNode.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class GraphNode {
+	private String location;
+	private HashMap<GraphNode, Integer> outboundEdges;
+	
+	// Alternative to using a map for the edges:
+	// private ArrayList<GraphNode> neighbors;
+	// private ArrayList<Integer> distances;
+	
+	
+
+}
